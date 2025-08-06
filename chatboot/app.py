84a1745daae91840ab84afe86a10d11b1297b7c7
fer3010 <fernando27926549@gmail.com>
2025-08-06 15:23:42 +0000

    Agregar lógica para manejar el envío de mensajes del usuario y la respuesta del bot en el chat

--- chatboot/app.py
+++ chatboot/app.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify, render_template
 
-app=Flask (__name__)
+app= Flask (__name__)
 
 def chatbot_response(user_message):
     user_message = user_message.lower().strip()
