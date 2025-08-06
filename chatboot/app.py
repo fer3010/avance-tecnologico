671be52cb61dddf96549b5f4fd42f02f9b461621
fer3010 <fernando27926549@gmail.com>
2025-08-06 14:55:30 +0000

    Agregar respuestas del chatbot para saludos, horarios, ubicación, contacto, precios y agradecimientos

--- chatboot/app.py
+++ chatboot/app.py
@@ -4,3 +4,17 @@ app=Flask (__name__)
 
 def chatbot_response(user_message):
     user_message = user_message.lower().strip()
+    if "hola" in user_message or "saludos" in user_message:
+        return "¡hola! ¿en que puedo ayudarte?"
+    elif "horarios" in user_message:
+        return "Los horarios de atención son de lunes a viernes de 9:00 a 18:00."
+    elif "ubicacion" in user_message:
+        return "Estamos ubicados en la calle Falsa 123, Ciudad."
+    elif "contacto" in user_message:
+        return "Puedes contactarnos al correo"
+    elif "precios" in user_message:
+        return "Los precios varían según el producto. Por favor, especifica el producto que te interesa."    
+    elif "gracias" in user_message or "gracias" in user_message:
+        return "¡De nada! Si tienes más preguntas, no dudes en preguntar."
+    else :
+        return "Lo siento, no entendí tu pregunta. ¿Podrías reformularla?"
